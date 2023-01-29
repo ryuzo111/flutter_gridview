65bf920665a116d81c384587884744d04e29c46e
ryuzo111 <yajima0401ryuzo@gmail.com>
2023-01-29 15:57:27 +0900

    feat: スクロールでfooterを縮まらせる実装

---
+++ lib/shrinkable_footer_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+import 'package:google_fonts/google_fonts.dart';
+
+class ShrinkableFooterPage extends StatefulWidget {
+  const ShrinkableFooterPage({Key? key}) : super(key: key);
+  // final String title;
+
+  @override
+  State<ShrinkableFooterPage> createState() => _ShrinkableFooterPage();
+}
+
+class _ShrinkableFooterPage extends State<ShrinkableFooterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
