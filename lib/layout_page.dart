1117dfcf5b0bba69672bbca67fe73e1bf962c040
ryuzo111 <yajima0401ryuzo@gmail.com>
2023-01-30 23:48:17 +0900

    fix ランドスケープの判定を追加

--- lib/layout_page.dart
+++ lib/layout_page.dart
@@ -22,86 +22,92 @@ class _ResponsiveDesignPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
+    final isPortrait = height > width; // ランドスケープの判定
     return Center(
-      child: Column(
-        children: <Widget>[
-          // const SizedBox(
-          //   height: 60,
-          // ),
-          Spacer(),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 50),
-            child: Text(
-              "flutter is best",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                height: 1.85,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          // const SizedBox(
-          //   height: 60,
-          // ),
-          Spacer(),
-          FlutterLogo(
-            size: height * 0.3,
-          ),
-          // const SizedBox(
-          //   height: 60,
-          // ),
-          Spacer(),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 50),
-            child: Text(
-              "fast development painge",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 15,
-                height: 1.85,
-              ),
-            ),
-          ),
-          // const SizedBox(
-          //   height: 60,
-          // ),
-          Spacer(),
-          ConstrainedBox(
-            constraints: BoxConstraints(
-              maxWidth: 650,
-            ),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                textStyle: TextStyle(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          maxWidth: 650,
+        ),
+        child: Column(
+          children: <Widget>[
+            // const SizedBox(
+            //   height: 60,
+            // ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: Text(
+                "flutter is best",
+                textAlign: TextAlign.center,
+                style: TextStyle(
                   color: Colors.white,
-                ),
-                primary: Theme.of(context).accentColor,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(4),
+                  fontSize: 16,
+                  height: 1.85,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              onPressed: () {},
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 15),
-                child: Text(
-                  'get started',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
+            ),
+            // const SizedBox(
+            //   height: 60,
+            // ),
+            Spacer(),
+            FlutterLogo(
+              size: height * 0.3,
+            ),
+            // const SizedBox(
+            //   height: 60,
+            // ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: Text(
+                "fast development painge",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  height: 1.85,
+                ),
+              ),
+            ),
+            // const SizedBox(
+            //   height: 60,
+            // ),
+            Spacer(),
+            ConstrainedBox(
+              constraints: BoxConstraints(
+                maxWidth: 650,
+              ),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  textStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                  primary: Theme.of(context).accentColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                ),
+                onPressed: () {},
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Text(
+                    'get started',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          // const SizedBox(
-          //   height: 60,
-          // ),
-          Spacer(),
-        ],
+            // const SizedBox(
+            //   height: 60,
+            // ),
+            Spacer(),
+          ],
+        ),
       ),
     );
   }
